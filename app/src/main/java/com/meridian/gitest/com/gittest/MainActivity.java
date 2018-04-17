8c68b5ea72a0d8ed642f99ea83b6f0817561035c
DESKTOP-V6J931A\libin <anvin@cybraum.net>
2018-04-17 12:08:11 +0530

    my third commit

--- app/src/main/java/com/meridian/gitest/com/gittest/MainActivity.java
+++ app/src/main/java/com/meridian/gitest/com/gittest/MainActivity.java
@@ -2,9 +2,10 @@ package com.meridian.gitest.com.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView tt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
